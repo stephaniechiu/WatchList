9420bd191465b07c1f04697bbc152e12d07136a3
Stephanie Chiu <stephaniechiu@outlook.com>
2020-04-20 18:04:46 -0700

    Initial Commit

---
+++ WatchList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchList
+//
+//  Created by Stephanie on 4/20/20.
+//  Copyright © 2020 Stephanie Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
